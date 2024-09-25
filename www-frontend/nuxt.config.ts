01d6abd6a848b61604e46d9c1c657aa3484842e1
Paweł Wilczek <5646246+beowoolf@users.noreply.github.com>
2024-09-25 06:19:02 +0200

    Update nuxt.config.ts

--- www-frontend/nuxt.config.ts
+++ www-frontend/nuxt.config.ts
@@ -1,49 +1,49 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-07-04",
+  compatibilityDate: '2024-04-03',
   css: [
-    "~/node_modules/bootstrap/dist/css/bootstrap.min.css",
-    "~/node_modules/bootstrap-icons/font/bootstrap-icons.css",
+    '~/node_modules/bootstrap/dist/css/bootstrap.min.css',
+    '~/node_modules/bootstrap-icons/font/bootstrap-icons.css',
   ],
   ssr: true,
-  devtools: { enabled: false },
+  devtools: { enabled: true },
   modules: [
-    "@nuxt/content",
-    "@nuxtjs/color-mode",
-    "@nuxt/image",
-    "@vite-pwa/nuxt",
-    "@nuxtjs/google-fonts",
-    "nuxt-jsonld",
-    "nuxt-gtag",
-    "nuxt-seo-experiments",
-    "@nuxtjs/robots",
-    "nuxt-simple-sitemap",
+    '@nuxt/content',
+    '@nuxtjs/color-mode',
+    '@nuxt/image',
+    '@vite-pwa/nuxt',
+    '@nuxtjs/google-fonts',
+    'nuxt-jsonld',
+    'nuxt-gtag',
+    'nuxt-seo-experiments',
+    '@nuxtjs/robots',
+    'nuxt-simple-sitemap',
   ],
   site: {
-    url: "https://mojezapiski.pl",
+    url: 'https://mojezapiski.pl',
   },
   gtag: {
-    id: "G-GMH8QEFZH6",
+    id: 'G-GMH8QEFZH6',
   },
   sitemap: {
-    siteUrl: "https://mojezapiski.pl",
+    siteUrl: 'https://mojezapiski.pl',
     trailingSlash: true,
     xsl: false,
     credits: false,
   },
   pwa: {
     manifest: {
-      name: "MojeZapiski.pl",
-      short_name: "MojeZapiski.pl",
-      theme_color: "#007bff",
-      lang: "pl-PL",
+      name: 'MojeZapiski.pl',
+      short_name: 'MojeZapiski.pl',
+      theme_color: '#007bff',
+      lang: 'pl-PL',
     },
     workbox: {
       offlineGoogleAnalytics: false,
     },
   },
   googleFonts: {
-    display: "swap",
+    display: 'swap',
     families: {
       Roboto: {
         wght: [400, 700],
@@ -53,23 +53,23 @@ export default defineNuxtConfig({
   app: {
     head: {
       htmlAttrs: {
-        lang: "pl-PL",
-        class: "h-100",
+        lang: 'pl-PL',
+        class: 'h-100',
       },
       headAttrs: {
         itemscope: true,
-        itemtype: "https://schema.org/WebPage",
+        itemtype: 'https://schema.org/WebPage',
       },
       bodyAttrs: {
-        class: "d-flex flex-column h-100",
+        class: 'd-flex flex-column h-100',
       },
-      charset: "utf-8",
-      viewport: "width=device-width, initial-scale=1",
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
       link: [],
       script: [
         {
           defer: true,
-          src: "//cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js",
+          src: '//cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
         },
       ],
     },
@@ -78,7 +78,7 @@ export default defineNuxtConfig({
     public: {
       trailingSlash: true,
       website: {
-        baseUrl: "https://mojezapiski.pl",
+        baseUrl: 'https://mojezapiski.pl',
       },
     },
   },
@@ -89,4 +89,4 @@ export default defineNuxtConfig({
       },
     },
   },
-});
+})
